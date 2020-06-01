0156c852474ea8a6956f7b980c005be6c66f5a7b
David Zwart (Rpi) <davidzwa@gmail.com>
2020-06-01 18:15:29 +0000

    Small path fix

--- mqtt-proxy/reg_service.sh
+++ mqtt-proxy/reg_service.sh
@@ -5,7 +5,7 @@ sudo cp /home/david/Moqiott/mqtt-proxy/mqtt-proxy.service ./mqtt-proxy.service
 echo "Stopping service (if existing). Chmod 644 service spec."
 sudo systemctl stop mqtt-proxy.service
 sudo chmod 644 /lib/systemd/system/mqtt-proxy.service
-chmod +x /home/david/mqtt-proxy/run.py
+chmod +x /home/david/Moqiott/mqtt-proxy/run.py
 
 echo "Restarting daemon, enabling/starting service."
 sudo systemctl daemon-reload
